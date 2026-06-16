9d8566515b389478a5d6066c8cac8bf2122cada0
Thereza Raquel <therezaraquelcoimbra@gmail.com>
2026-06-16 20:15:48 -0300

    iniciando a automação do teste de pedido aprovado

---
+++ playwright/e2e/pedidos.spec.ts
@@ -0,0 +1,27 @@
+import { test, expect } from '@playwright/test'
+
+test('deve consultar um pedido aprovado', async ({ page }) => {
+  await page.goto('http://localhost:5173/')
+  
+  //checkpoints
+  await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
+
+  await page.getByRole('link', { name: 'Consultar Pedido' }).click()
+
+  //checkpoints
+  await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
+
+  await page.getByTestId('search-order-id').fill('VLO-VDAOST')
+
+  await page.getByTestId('search-order-button').click()
+
+  await expect(page.getByTestId('order-result-id')).toBeVisible()
+  await expect(page.getByTestId('order-result-id')).toContainText('VLO-VDAOST')
+
+  await expect(page.getByTestId('order-result-status')).toBeVisible()
+  await expect(page.getByTestId('order-result-status')).toContainText('APROVADO')
+
+
+})
+
+
