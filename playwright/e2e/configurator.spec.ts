e20d955f18da9d959615897493b604db36caf6de
Thereza Raquel <therezaraquelcoimbra@gmail.com>
2026-09-01 20:07:47 -0300

    atualizando CT03 mudando o título

--- playwright/e2e/configurator.spec.ts
+++ playwright/e2e/configurator.spec.ts
@@ -35,7 +35,7 @@ test.describe('Configuração do Veículo', () => {
         await app.configurator.validateCarImage('/src/assets/glacier-blue-aero-wheels.png')
     })
 
-    test('CT03 - deve calcular o preço dinâmico ao adicionar e remover opcionais', async ({ app }) => {
+    test('Deve calcular o preço dinâmico ao adicionar e remover opcionais', async ({ app }) => {
         // Arrange - Elementos encapsulados via Feature Actions
         const { precisionPark, fluxCapacitor } = app.configurator.elements
 
